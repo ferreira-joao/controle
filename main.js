b0dd1f7a2649e04a62293c912d39d48f237697b8
ferreira-joao <61431659+ferreira-joao@users.noreply.github.com>
2020-03-22 17:09:05 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      window.location = "principal.html";
+    } else {
+      // No user is signed in.
+    }
+});
+
+function login() {
+    var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("password_field").value;
+
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+        var errorMessage = error.message;
+
+        window.alert("Erro: "+errorMessage);
+    });
+}
+
+function logOut() {
+    firebase.auth().signOut()
+    .then(function(){
+      window.location = "index.html";
+    })
+    .catch(function(error){
+      window.alert("Erro: "+error);
+    })
+    
+}

8e8d02d392066a48afd45fe31498990bea1c8aba
ferreira-joao <klausblack231@gmail.com>
2020-03-23 11:10:23 -0300

    Criado estrutura da página pricipal

--- login.js
+++ login.js
@@ -1,12 +1,12 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyDXw2DU-QFbtqGTOjtDgZLHKSZQE8Q5Na8",
-    authDomain: "controle-equip.firebaseapp.com",
-    databaseURL: "https://controle-equip.firebaseio.com",
-    projectId: "controle-equip",
-    storageBucket: "controle-equip.appspot.com",
-    messagingSenderId: "429370847069",
-    appId: "1:429370847069:web:e8c94ff03cd939b65f02b4"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyDXw2DU-QFbtqGTOjtDgZLHKSZQE8Q5Na8",
+  authDomain: "controle-equip.firebaseapp.com",
+  databaseURL: "https://controle-equip.firebaseio.com",
+  projectId: "controle-equip",
+  storageBucket: "controle-equip.appspot.com",
+  messagingSenderId: "429370847069",
+  appId: "1:429370847069:web:e8c94ff03cd939b65f02b4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
--- main.js
+++ main.js
@@ -1,32 +1,31 @@
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-      
-    } else {
-      // No user is signed in.
-    }
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // User is signed in.
+  } else {
+    // No user is signed in.
+  }
 });
 
 function login() {
-    var userEmail = document.getElementById("email_field").value;
-    var userPass = document.getElementById("password_field").value;
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
 
-    firebase.auth().signInWithEmailAndPassword(userEmail, userPass)
-    .then(function(){
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass)
+    .then(function () {
       window.location = "principal.html";
-    }).catch(function(error) {
-        var errorMessage = error.message;
-        window.alert("Erro: "+errorMessage);
+    }).catch(function (error) {
+      var errorMessage = error.message;
+      window.alert("Erro: " + errorMessage);
     });
 }
 
 function logOut() {
-    firebase.auth().signOut()
-    .then(function(){
+  firebase.auth().signOut()
+    .then(function () {
       window.location = "index.html";
     })
-    .catch(function(error){
-      window.alert("Erro: "+error);
+    .catch(function (error) {
+      window.alert("Erro: " + error);
     })
-    
+
 }
